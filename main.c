673ac1f63e98b25e94661b8f11c4f3befe6b308b
mouhammadhamsho <mouhammad.hamshoo@gmail.com>
2019-05-22 13:32:52 +0200

    sometinh

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int sum(int *arr[],int size , int Thresh)
+{
+	int sum=0;
+	
+	for (int i=0 ; i<size ; ++i)
+	{
+		sum=sum+arr[size];
+	}
+	return sum;
+}
+
+
+
+
+int scale(float value)
+{
+	return value*200;
+	
+}
+
+int main_()
+
+{
+	
+
+	
+}
